46a8cfd2ebf7d1fc86b6e611a3d8c085c116fb72
anonymous-gtx <111691847+anonymous-gtx@users.noreply.github.com>
2024-03-01 22:32:43 +0530

    signup_page.dart

--- signup_page.dart
+++ signup_page.dart
@@ -1,101 +1,153 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:askme/pallete.dart';
 
-class SignupPage extends StatelessWidget {
+class SignUP extends StatefulWidget {
+  final void Function()? onPressed;
+  const SignUP({Key? key, required this.onPressed}) : super(key: key);
+
+  @override
+  State<SignUP> createState() => _SignUPState();
+}
+
+class _SignUPState extends State<SignUP> {
+  bool _obscureText = true;
+  final _formkey = GlobalKey<FormState>();
+  bool isLoading = false;
+  final TextEditingController _email = TextEditingController();
+  final TextEditingController _password = TextEditingController();
+  final TextEditingController _confirmPassword =
+      TextEditingController(); // Added
+  createUserWithEmailAndPassword() async {
+    try {
+      setState(() {
+        isLoading = true;
+      });
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: _email.text,
+        password: _password.text,
+      );
+      setState(() {
+        isLoading = false;
+      });
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        isLoading = false;
+      });
+      if (e.code == 'weak-password') {
+        return ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text("The password provided is too weak.")),
+        );
+      } else if (e.code == 'email-already-in-use') {
+        return ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text("The account already exists.")),
+        );
+      }
+    } catch (e) {
+      setState(() {
+        isLoading = false;
+      });
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Pallete.mainFontColor,
       appBar: AppBar(
-        title: Text('Sign Up'),
-        backgroundColor: Pallete.whiteColor,
+        centerTitle: true,
+        title: const Text("SignUP"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            // Email Section
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: 'Email',
-                filled: true,
-                fillColor: Pallete.firstSuggestionBoxColor,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide: BorderSide.none,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Form(
+            key: _formkey,
+            child: OverflowBar(
+              overflowSpacing: 20,
+              children: [
+                TextFormField(
+                  controller: _email,
+                  validator: (text) {
+                    if (text == null || text.isEmpty) {
+                      return 'Email is empty';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(labelText: "Email"),
                 ),
-              ),
+                TextFormField(
+                  controller: _password,
+                  validator: (text) {
+                    if (text == null || text.isEmpty) {
+                      return 'Password is empty';
+                    }
+                    return null;
+                  },
+                  obscureText: _obscureText,
+                  decoration: InputDecoration(
+                    labelText: "Password",
+                    suffixIcon: IconButton(
+                      icon: Icon(_obscureText
+                          ? Icons.visibility_off
+                          : Icons.visibility),
+                      onPressed: () {
+                        setState(() {
+                          _obscureText = !_obscureText;
+                        });
+                      },
+                    ),
+                  ),
+                ),
+                TextFormField(
+                  controller: _confirmPassword,
+                  validator: (text) {
+                    if (text == null || text.isEmpty) {
+                      return 'Confirm Password is empty';
+                    } else if (text != _password.text) {
+                      return 'Passwords do not match';
+                    }
+                    return null;
+                  },
+                  obscureText: _obscureText,
+                  decoration: InputDecoration(
+                    labelText: "Confirm Password",
+                    suffixIcon: IconButton(
+                      icon: Icon(_obscureText
+                          ? Icons.visibility_off
+                          : Icons.visibility),
+                      onPressed: () {
+                        setState(() {
+                          _obscureText = !_obscureText;
+                        });
+                      },
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  height: 45,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formkey.currentState!.validate()) {
+                        createUserWithEmailAndPassword();
+                      }
+                    },
+                    child: isLoading
+                        ? const CircularProgressIndicator(
+                            color: Colors.white,
+                          )
+                        : const Text("SignUp"),
+                  ),
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  height: 45,
+                  child: ElevatedButton(
+                      onPressed: widget.onPressed, child: const Text("Login")),
+                ),
+              ],
             ),
-            SizedBox(height: 16.0),
-
-            // Password Section
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: 'Password',
-                filled: true,
-                fillColor: Pallete.secondSuggestionBoxColor,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide: BorderSide.none,
-                ),
-              ),
-              obscureText: true,
-            ),
-            SizedBox(height: 16.0),
-
-            // Confirm Password Section
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: 'Confirm Password',
-                filled: true,
-                fillColor: Pallete.secondSuggestionBoxColor,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  borderSide: BorderSide.none,
-                ),
-              ),
-              obscureText: true,
-            ),
-            SizedBox(height: 16.0),
-
-            // Signup Button
-            ElevatedButton(
-              onPressed: () {
-                // Add your signup logic here
-              },
-              style: ElevatedButton.styleFrom(
-                primary: Pallete.thirdSuggestionBoxColor,
-                padding: EdgeInsets.all(16.0),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-              ),
-              child: Text(
-                'Sign Up',
-                style: TextStyle(
-                  color: Pallete.mainFontColor,
-                  fontSize: 16.0,
-                ),
-              ),
-            ),
-            SizedBox(height: 16.0),
-
-            // Login Option
-            GestureDetector(
-              onTap: () {
-                // Navigate to the login page
-                Navigator.pop(context); // Pop the current page
-              },
-              child: Text(
-                'Already have an account? Login',
-                style: TextStyle(
-                  color: Pallete.assistantCircleColor,
-                ),
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
